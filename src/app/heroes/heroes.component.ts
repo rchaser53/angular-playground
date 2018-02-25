59b1aa5fbad36ca269dda00cbaf94f8885f3335d
rchaser53 <tayoshizawa29@gmail.com>
2018-02-25 17:09:28 +0900

    try to use some ng syntaxes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  // styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
-  constructor() { }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
